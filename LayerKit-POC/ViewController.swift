10f3e0d70d1bb998669ee3bca33f4334c0cbc158
0xLeif <zmeriksen@icloud.com>
2020-12-22 19:02:57 -0600

    init

--- LayerKit-POC/ViewController.swift
+++ LayerKit-POC/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import LayerKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let bounds = view.bounds
+        
+        let width: CGFloat = bounds.width - 32
+        let height: CGFloat = bounds.height - 32
+        let x: CGFloat = 16
+        let y: CGFloat = 16
+        
+        view.layer
+            .background(color: UIColor.cyan.cgColor)
+            .embed {
+                Layer(backgroundColor: UIColor.red.cgColor)
+                    .frame(x: x, y: y, width: width, height: height)
+                    .corner(radius: 32)
+                    .masked(corners: [.layerMaxXMaxYCorner, .layerMaxXMinYCorner])
+                    .embed {
+                        Layer(backgroundColor: UIColor.gray.cgColor)
+                            .frame(x: 50, y: 400, width: 100, height: 30)
+                            .corner(radius: 16)
+                    }
+            }
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print(#function)
+        
+        let bounds = view.bounds
+        
+        let width: CGFloat = bounds.width - 32
+        let height: CGFloat = bounds.height - 32
+        
+        let x: CGFloat = 16
+        let y: CGFloat = 16
+        
+        
+        view.layer.sublayers?.first?.frame(x: x, y: y, width: width, height: height)
+    }
 }
-
